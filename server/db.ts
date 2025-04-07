879c8834c4f0acf70bb4a7a8f04501c9a8be41bc
Pedro Henrique Simões Baião <pedro.baiao@pentare.com.br>
2025-04-07 15:11:52 -0300

    Adiciona configurações SSL para conexão com o banco de dados

--- server/db.ts
+++ server/db.ts
@@ -3,6 +3,13 @@ import postgres from 'postgres';
 import * as schema from "@shared/schema";
 
 // Utilizando os dados do banco de dados
-const queryClient = postgres(process.env.DATABASE_URL!);
+const queryClient = postgres(process.env.DATABASE_URL!, {
+  ssl: {
+    rejectUnauthorized: false
+  },
+  max: 20,
+  idle_timeout: 30,
+  connect_timeout: 10
+});
 
 export const db = drizzle(queryClient, { schema });
