39ea9b63036f4ec1292895d4ffd8cea0d80ddc6d
Pedro Henrique Simões Baião <pedro.baiao@pentare.com.br>
2025-04-08 08:30:50 -0300

    Adiciona mais informações de erro no middleware de tratamento de erros

--- server/index.ts
+++ server/index.ts
@@ -42,9 +42,14 @@ app.use((req, res, next) => {
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
     const message = err.message || "Internal Server Error";
+    const stack = process.env.NODE_ENV === 'development' ? err.stack : undefined;
 
-    res.status(status).json({ message });
-    throw err;
+    console.error('Error:', err);
+    res.status(status).json({ 
+      message,
+      error: err.name,
+      stack
+    });
   });
 
   // importantly only setup vite in development and after
