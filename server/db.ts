91f7d1c838cb4ea82c82b78ea3b4d13403062fe7
Pedro Henrique Simões Baião <pedro.baiao@pentare.com.br>
2025-04-08 08:21:30 -0300

    Altera configuração do banco de dados para usar o módulo pg

--- server/db.ts
+++ server/db.ts
@@ -1,15 +1,16 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from 'postgres';
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 import * as schema from "@shared/schema";
 
-// Utilizando os dados do banco de dados
-const queryClient = postgres(process.env.DATABASE_URL!, {
+// Configuração do pool PostgreSQL
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   },
   max: 20,
-  idle_timeout: 30,
-  connect_timeout: 10
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 10000
 });
 
-export const db = drizzle(queryClient, { schema });
+export const db = drizzle(pool, { schema });
